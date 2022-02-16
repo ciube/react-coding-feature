1dd28b5fcd2b1cdf1e53bf53739a5be3c388dbbc
Matteo Langone <matteo.langone@gmail.com>
2022-02-16 16:37:12 +0100

    created dashboard and preferences pages. created login form and sessionStorage token

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./Pages/Dashboard";
+import Preferences from "./Pages/Preferences";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+
+import { Dropdown, Row, Col } from "react-bootstrap";
+
+function setToken(userToken) {
+  console.log(userToken)
+  sessionStorage.setItem('token', userToken);
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  return tokenString;
+}
 
 function App() {
+
+  const token = getToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Row>
+        <Col>
+          <h1>Application</h1>
+        </Col>
+        <Col md={"auto"}>
+          {token ? (
+            <Dropdown>
+              <Dropdown.Toggle variant="info" id="dropdown-basic">
+                Profile
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="/dashboard">Dashboard</Dropdown.Item>
+                <Dropdown.Item href="/preferences">Preferences</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : (
+            <a className="btn btn-primary" href="/login">
+              Login
+            </a>
+          )}
+        </Col>
+      </Row>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={token ? <Dashboard /> : <Home />} />
+          <Route
+            path="/preferences"
+            element={token ? <Preferences /> : <Home />}
+          />
+          <Route path="/login" element={<Login setToken={setToken} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
